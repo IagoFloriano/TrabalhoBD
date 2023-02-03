73baad7c1c97b0882aea1197be92c8d9f5f5123f
Iago Floriano <iagomfloriano@gmail.com>
2023-02-03 14:12:26 -0300

    Faz leitura

--- escalona.cpp
+++ escalona.cpp
@@ -19,14 +19,28 @@ int main(){
   while(cin >> time >> task >> type >> var) {
     cin.ignore();
     operacao_t op = operacao_t(time, type, var);
-    activeTasks.inset(task);
+    activeTasks.insert(task);
 
     // if first operation add to map
     if(!taskIndex.count(task)){
+      tarefa_t newtask {};
+      newtask.id = task;
+      newtask.ops = std::vector<operacao_t>(); 
+      tasks.push_back(newtask);
+      taskIndex[task] = tasks.size() - 1;
     }
+    if(type == 'C') activeTasks.erase(task);
+
+    tasks[taskIndex[task]].ops.push_back(op);
 
     if(activeTasks.empty()){
       // do tests
+      for(tarefa_t t: tasks){
+        cout << t.id << ":" << endl;
+        for(operacao_t o: t.ops){
+          cout << std::get<0>(o) << " " << std::get<1>(o) << " " << std::get<2>(o) << endl;
+        }
+      }
 
       // reset variables
       activeTasks.clear();
